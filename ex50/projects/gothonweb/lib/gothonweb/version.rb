a959031ae85ca17aaa8dfa33fed3c0a52e56effe
cwy007 <chanweiyan007@gmail.com>
2018-02-27 00:47:29 +0800

    Exercise 51: Getting Input from a Browser

---
+++ ex50/projects/gothonweb/lib/gothonweb/version.rb
@@ -0,0 +1,3 @@
+module GOTHONWEB
+  VERSION = "1.0.0"
+end
--- ex50/projects/gothonweb/tests/test_gothonweb.rb
+++ ex50/projects/gothonweb/tests/test_gothonweb.rb
@@ -1,10 +1,29 @@
-require "./lib/gothonweb.rb"
-require "test/unit"
+require './bin/app'
+require 'test/unit'
+require 'rack/test'
 
-class GothonwebTests < Test::Unit::TestCase
+class MyAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
 
-  def test_sample
-    assert_equal("5", 5.to_s)
+  def app
+    Sinatra::Application
+  end
+
+  def test_my_default
+    get '/'
+    assert_equal 'Hello world', last_response.body
+  end
+
+  def test_hello_form
+    get '/hello/'
+    assert last_response.ok?
+    assert last_response.body.include?('A Greeting')
+  end
+
+  def test_hello_form_post
+    post '/hello/', params={:name => 'Frank', :greeting => 'Hi'}
+    assert last_response.ok?
+    assert last_response.body.include?('I just wanted to say')
   end
 
 end
